d72e050be05cf03517bff46882bc994664cf632a
Alessio Rocchi <alessio@vicarious.com>
2019-01-04 13:58:33 -0800

    fixed kdl_env_unit.cpp tests by using setActiveCollisionObjects() for continuous contact manager

--- tesseract_ros/test/kdl_env_unit.cpp
+++ tesseract_ros/test/kdl_env_unit.cpp
@@ -58,6 +58,7 @@ protected:
     ccm = env->getContinuousContactManager();
     std::vector<tesseract::ContactResultMap> collisions;
     tesseract::BasicKinConstPtr kin = env->getManipulator("manipulator");
+    ccm->setActiveCollisionObjects(kin->getLinkNames());
     bool found = tesseract::continuousCollisionCheckTrajectory(*ccm, *env, *kin, traj, collisions);
     return found;
   }
